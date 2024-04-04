02a140b8034fea7a021b6cf71aa737ff580c615f
Edgar <edgara.mendozaf@gmail.com>
2024-04-04 11:27:40 -0600

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+      setCategories([...categories, 'Valorant']);
+  }
+
+  return (
+    <>
+        {/* TITULO */}
+        <h1>GifExpertApp</h1>
+
+        {/* INPUT */}
+
+        {/* LISTADO DE GIF */}
+        <button onClick={ onAddCategory }>Agregar</button>
+        <ol>
+          { categories.map( category => {
+                    return <li key={ category }>{ category }</li>
+                })
+          }
+        </ol>
+            {/* GIF ITEM */}
+    </>
+  )
+}
