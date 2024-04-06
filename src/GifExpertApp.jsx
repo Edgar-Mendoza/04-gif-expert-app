c4edcbe46d8b3dc7a904e77fb626a3ba80eca1c7
Edgar <edgara.mendozaf@gmail.com>
2024-04-05 19:37:52 -0600

    onNewCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 
 export const GifExpertApp = () => {
 
   const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
 
-  const onAddCategory = () => {
-      setCategories([...categories, 'Valorant']);
+  const onAddCategory = ( newCategory ) => {
+    setCategories([ newCategory, ...categories ]);
   }
 
   return (
@@ -14,9 +16,9 @@ export const GifExpertApp = () => {
         <h1>GifExpertApp</h1>
 
         {/* INPUT */}
+        <AddCategory onNewCategory={ onAddCategory }/>
 
         {/* LISTADO DE GIF */}
-        <button onClick={ onAddCategory }>Agregar</button>
         <ol>
           { categories.map( category => {
                     return <li key={ category }>{ category }</li>
