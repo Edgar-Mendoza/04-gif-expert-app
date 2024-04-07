1e06a8678227673dedf1bac5d00bac51383fa1df
Edgar <edgara.mendozaf@gmail.com>
2024-04-07 15:12:02 -0600

    getGfis

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,31 +1,30 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
-
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
-
+  const [ categories, setCategories ] = useState([ 'Dragon Ball' ]);
+  
   const onAddCategory = ( newCategory ) => {
+    if ( categories.includes(newCategory) ) return;
     setCategories([ newCategory, ...categories ]);
   }
 
   return (
     <>
-        {/* TITULO */}
         <h1>GifExpertApp</h1>
 
-        {/* INPUT */}
         <AddCategory onNewCategory={ onAddCategory }/>
-
-        {/* LISTADO DE GIF */}
-        <ol>
-          { categories.map( category => {
-                    return <li key={ category }>{ category }</li>
-                })
-          }
-        </ol>
-            {/* GIF ITEM */}
+        
+        { 
+          categories.map( ( category ) => (
+            <GifGrid 
+              key={ category } 
+              category= { category }
+            />
+          ))
+        }
     </>
   )
 }
